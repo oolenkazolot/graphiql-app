285a5dee5b6abb1b887875c3879f3fd63cf2ff1d
Svetik-K <sviatlana_kliautsova@icloud.com>
2023-05-05 10:48:59 +0200

    feat: add form hook for Sign Up page

--- src/pages/signUpPage/SignUpPage.tsx
+++ src/pages/signUpPage/SignUpPage.tsx
@@ -1,18 +1,32 @@
 import { Link } from 'react-router-dom';
 import './signUpPage.scss';
+import { useForm } from 'react-hook-form';
+
+interface FormValues {
+  email: string;
+  password: string;
+}
 
 const SignUpPage: React.FC = () => {
+  const {register, handleSubmit, formState: { errors }} = useForm<FormValues>();
+
+  const handleFormSubmit = (data: FormValues)=> {
+    console.log(data);
+  }
+
   return (
     <div className="sign-up">
-      <form className="sign-up-form">
+      <form className="sign-up-form" onSubmit={handleSubmit(handleFormSubmit)}>
         <h1 className="sign-up-form__title">Sign Up</h1>
         <div className="sign-up-form__email-info">
           <label htmlFor="user-email">E-mail</label>
-          <input type="email" name="email" id="user-email" placeholder="Please enter your e-mail"/>
+          <input type="email" id="user-email" {...register("email", { required: "This field is mandatory for registration" })} placeholder="Please enter your e-mail"/>
+          {errors.email?.type === "required" && <span className="error-message">{errors.email.message}</span>}
         </div>
         <div className="sign-up-form__password-info">
           <label htmlFor="user-pass">Password</label>
-          <input type="password" name="password" id="user-pass" placeholder="Please enter your password"/>
+          <input type="password" id="user-pass" {...register("password", { required: "This field is mandatory for registration" })} placeholder="Please enter your password"/>
+          {errors.password?.type === "required" && <span className="error-message">{errors.password.message}</span>}
         </div>
         <button className="button button_sign-up">Submit</button>
         <div className="sign-up-form__redirect">
